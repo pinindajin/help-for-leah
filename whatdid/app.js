fa6f06e0466075362236e2ec4f6a6cec453adb0f
Jake McBride <pinindajin@gmail.com>
2018-07-04 20:20:15 -0600

    bodyparser and post

--- whatdid/app.js
+++ whatdid/app.js
@@ -1,13 +1,38 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 
 const app = express();
+app.use(bodyparser.json()); // for parsing application/json
+app.use(bodyparser.urlencoded({
+  extended: true
+})); // for parsing application/x-www-form-urlencoded
+
+let things = [
+  'Kissed Puppy',
+  'Tooted Louadly',
+  'Ran 50 Miles',
+  'Cooked Dinner',
+  'Ate Dinner',
+  'Drank Tasty Wine'
+];
 
 app.get('/', (req, res) => {
   res.json('DEFAULT ROUTE');
 });
 
 app.get('/api/things', (req, res) => {
-  res.json('hello myself');
+  res.json({
+    things
+  });
+});
+
+app.post('/api/things', (req, res) => {
+  const thing = req.body.thing;
+  console.log(`Adding a thing ... ${thing}`)
+  things.push(thing);
+  res.json({
+    things
+  })
 });
 
 app.listen(3000, () => {
